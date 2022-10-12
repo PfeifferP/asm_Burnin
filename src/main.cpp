6c1c2cec56a9701d43c182a5b6f12f1ed037d304
PfeifferP <admin@pfeiffer-privat.de>
2022-10-12 13:53:04 +0200

    first test

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,35 @@
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 HardwareSerial ASM(2);
 Ticker status;
-int step, count;
-String steps[5] = {"Mess", "Stop", "Vent", "Cal ", "blan"};
+int loops, step, count;
+String steps[5] = {"Messen", "Stop", "Belüften", "Kalibrieren ", "blan"};
+String cmds[5] = {"=CYE", "=CYD", "=IVE", "!AC", ""};
+int counts[5] = { 90, 5, 5, 10, 4};
+
+String gettime (int t) {
+  String st = "";
+  int m = (t/60);
+  int s = (t - (m*60));
+  if (m<10) {st = "0";}
+  st = st + String(m)+":";
+  if (s<10){st=st+"0";}
+  st = st + String(s);
+  return st;
+}
 
 void periodicStatus() {
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Step: ");
   lcd.print(steps[step]);
-  lcd.setCursor(0,2);
-  lcd.print(count);
-  count++;
-  if (count >= 10) {
-    count = 0;
+  lcd.setCursor(15,0);
+  lcd.print(gettime(count));
+  
+  count--;
+  if (count < 1) {
     step++;
-  }
-  if (step >= 4) {
-    step = 0;
+    if (step >= 4) { step = 0; }
+    count = counts[step];
+    Serial.println(cmds[step]);
   }
 }
 
@@ -34,6 +46,9 @@ void setup()
   lcd.backlight();
   Serial.begin(9600);
   ASM.begin(9600, SERIAL_8N1, 16, 17);
+  loops = 5;
+  count = counts[0];
+  Serial.println(cmds[0]);
   status.attach_ms(1000, periodicStatus);
 }
 
